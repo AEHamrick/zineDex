33b959c3e1d444ca54bdd46e26bb74efba96956b
Evan <crawling.chaos@gmail.com>
2018-02-24 23:15:57 -0500

    Initial commit

---
+++ classes.py
@@ -0,0 +1,80 @@
+class RefItem(object):
+
+	def __init__(self, reftype, refname, refby, refbypage, refbyissue):
+		self.type = reftype
+		self.name = refname
+		self.referencedBy = refby
+		self.referencedByIssue = refbyissue
+		self.referencedOnPage = refbypage
+
+
+class Album(RefItem):
+
+	def __init__(self, refname, refby, refbypage, refbyissue,
+				 title, refband, genre, reflabel=None, year=None):
+		super().__init__('Album', refname, refby, refbypage, refbyissue)
+
+		#TODO: this might work better than passing the type in?
+		#self.type = self.__class__.__name__
+
+		self.title = title
+		self.band = refband
+		self.genre = genre
+		self.label = reflabel
+		self.year = year
+
+
+class Band(RefItem):
+
+	def __init__(self, refname, refby, refbypage, refbyissue,
+				 name, location=None, year=None):
+		super().__init__('Band', refname, refby, refbypage, refbyissue)
+
+		self.name = name
+		self.location = location
+		self.year = year
+
+
+class Interview(RefItem):
+
+	def __init__(self, refname, refby, refbypage, refbyissue,
+				 name, interviewer, type):
+		super().__init__('Interview', refname, refby, refbypage, refbyissue)
+
+		self.name = name
+		self.interviewer = interviewer
+		self.type = type
+
+
+class Zine(RefItem):
+
+	def __init__(self, refname, refby, refbypage, refbyissue,
+				 name, editor=None, location=None, web=None):
+		super().__init__('Zine', refname, refby, refbypage, refbyissue)
+
+		self.name = name
+		self.location = location
+		self.editor = editor
+		self.web = web
+
+
+class Label(RefItem):
+
+	def __init__(self, refname, refby, refbypage, refbyissue,
+				 name, location=None, web=None):
+		super().__init__('Label', refname, refby, refbypage, refbyissue)
+
+		self.name = name
+		self.location = location
+		self.web = web
+
+class Festival(RefItem):
+
+	def __init__(self, refname, refby, refbypage, refbyissue,
+				 name, year = None, location=None, web=None):
+		super().__init__('Festival', refname, refby, refbypage, refbyissue)
+
+		self.name = name
+		self.location = location
+		self.web = web
+		self.year = year
